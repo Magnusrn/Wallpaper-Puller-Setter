00a28a19c13f474b5c075c83e791e82ce3bb29cb
Magnusrn <Magnusn@hotmail.co.uk>
2023-01-21 03:55:41 +0000

    replace desktop setting method to osascript

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import praw
 import requests
 import os
 import time
+import subprocess
 import hashlib
 import glob
 from secrets import CLIENT_ID, CLIENT_SECRET, USER_AGENT
@@ -28,42 +29,49 @@ def main(subreddit):
         print("No .jpg images in the r/%s subreddit" % subreddit)
         quit()
 
-    def md5Checksum(filePath, url):
-        m = hashlib.md5()
-        if url is None:
-            with open(filePath, 'rb') as fh:
-                m = hashlib.md5()
-                while True:
-                    data = fh.read(8192)
-                    if not data:
-                        break
-                    m.update(data)
-                return m.hexdigest()
-        else:
-            r = requests.get(url)
-            for data in r.iter_content(8192):
-                m.update(data)
-            return m.hexdigest()
+    # def md5Checksum(filePath, url):
+    #     m = hashlib.md5()
+    #     if url is None:
+    #         with open(filePath, 'rb') as fh:
+    #             m = hashlib.md5()
+    #             while True:
+    #                 data = fh.read(8192)
+    #                 if not data:
+    #                     break
+    #                 m.update(data)
+    #             return m.hexdigest()
+    #     else:
+    #         r = requests.get(url)
+    #         for data in r.iter_content(8192):
+    #             m.update(data)
+    #         return m.hexdigest()
+    #
+    # def images_identical():  # compares local image to remote
+    #     if md5Checksum(image_path, None) == md5Checksum(None, remote_url):
+    #         print("images identical")
+    #         return True
+    #     return False
+    #
+    # if len(glob.glob(os.path.dirname(__file__) + "/*.jpg")) != 0:
+    #     if not images_identical():
+    #         print("Local image differs to remote, updating...")
+    #     else:
+    #         print("Local image is the same as remote.")
+    #         return
+    # else:
+    #     print("no file in folder")
 
-    def images_identical():  # compares local image to remote
-        if md5Checksum(image_path, None) == md5Checksum(None, remote_url):
-            print("images identical")
-            return True
-        return False
-
-    if len(glob.glob(os.path.dirname(__file__) + "/*.jpg")) != 0:
-        if not images_identical():
-            print("Local image differs to remote, updating...")
-        else:
-            print("Local image is the same as remote.")
-            return
-    else:
-        print("no file in folder")
+    def set_desktop_background():
+        abspath = os.path.abspath("wallpaper.jpg")
+        # what the fuck is this apostrophe nightmare
+        script = f"""osascript -e 'tell application "Finder" to set desktop picture to POSIX file "{abspath}"'"""
+        subprocess.Popen(script, shell=True)
 
     with open(image_path, "wb") as f:
         f.write(file.content)
         try:
-            app('Finder').desktop_picture.set(mactypes.File(image_path))
+            print("setting desktop wallpaper")
+            set_desktop_background()
         except Exception as e:
             print(e)
 
@@ -81,4 +89,4 @@ while True:
         main(subreddit.lower())
     except Exception as e:
         print(e)
-    time.sleep(900)  # seconds
+    time.sleep(10)  # seconds
